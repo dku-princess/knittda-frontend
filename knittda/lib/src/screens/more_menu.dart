8d813d39ea3a889c750bbd07d1738da5ae55f9d6
jungHY <jungHY@dankook.ac.kr>
2025-04-24 23:40:29 +0900

    feat: 더보기 화면

--- knittda/lib/src/screens/more_menu.dart
+++ knittda/lib/src/screens/more_menu.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:knittda/src/component/more_menu_list.dart';
 
 class MoreMenu extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('more menu'),
+      appBar: AppBar(
+        title: Padding(
+          padding: EdgeInsets.only(left: 8.0, top: 8.0), // 원하는 만큼 조절 가능
+          child: Text(
+            "더보기",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          MoreMenuList(title: '로그아웃'),
+          MoreMenuList(title: '문의하기'),
+          MoreMenuList(title: '회원탈퇴'),
+        ],
       ),
     );
+
   }
 }
