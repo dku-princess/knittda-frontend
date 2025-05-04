9fa5582476ac5c5e99c91702074ac623fddca2ca
jungHY <jungHY@dankook.ac.kr>
2025-05-04 23:00:14 +0900

    refactor: user page 수정

--- knittda/lib/src/screens/mypage.dart
+++ knittda/lib/src/screens/mypage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:knittda/src/widgets/listitems/user_mypage.dart';
 
 class Mypage extends StatelessWidget {
   @override
@@ -17,7 +16,70 @@ class Mypage extends StatelessWidget {
           ),
         ),
       ),
-      body: UserMypage(name: "홍길동", email: "loveyou@dankook.ac.kr"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, //가로방향: 좌측정렬
+          children: [
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.only(top: 16, bottom: 16),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.shade300), //bottom 외각선
+                ),
+              ),
+              child: Row(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.grey,
+                      ),
+                      const SizedBox(width: 12),
+                      Text(
+                        "홍길동",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  TextButton(
+                    onPressed: (){
+                      //프로필 수정으로 이동
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.grey[300],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: const BorderRadius.all(Radius.circular(8)),
+                      ),
+                    ),
+                    child: Text(
+                      '프로필 수정',
+                      style: TextStyle(color: Colors.grey[600], fontSize: 14),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 26),
+            // 연결된 계정
+            Text(
+              "연결된 계정",
+              style: TextStyle(
+                fontSize: 16,
+                //fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              "test@test.com",
+            ),
+          ],
+        ),
+      )
     );
   }
 }
