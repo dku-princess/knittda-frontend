9eefed8aa529e321a7ec58f1491374424d7490db
jungHY <jungHY@dankook.ac.kr>
2025-05-05 15:58:04 +0900

    refactor: workdetails 수정

--- knittda/lib/src/screens/work_details.dart
+++ knittda/lib/src/screens/work_details.dart
@@ -19,7 +19,7 @@ class WorkDetails extends StatelessWidget {
     final topPadding = MediaQuery.of(context).padding.top; //상태바 높이
 
     return DefaultTabController(
-      length: 3,
+      length: myTabs.length,
       child: Scaffold(
         body: NestedScrollView(
           headerSliverBuilder: (context, innerBoxIsScrolled) {
@@ -27,12 +27,12 @@ class WorkDetails extends StatelessWidget {
               SliverAppBar(
                 pinned: true, //appbar 고정
                 expandedHeight: 210.0, //확장 높이
-                backgroundColor: Colors.white,
-                leading: IconButton(
+                backgroundColor: Colors.white, //배경 흰색
+                leading: IconButton( //뒤로가기 버튼
                   icon: Icon(Icons.arrow_back, color: Colors.black),
                   onPressed: () {},
                 ),
-                flexibleSpace: FlexibleSpaceBar(
+                flexibleSpace: FlexibleSpaceBar( //확장영역
                   background: Padding(
                     padding: EdgeInsets.only(top: topPadding + 56, left: 24),
                     child: Row(
@@ -49,9 +49,9 @@ class WorkDetails extends StatelessWidget {
                   ),
                 ),
                 bottom: TabBar(
-                  indicatorColor: Colors.black87,
-                  labelColor: Colors.black87,
-                  unselectedLabelColor: Colors.grey,
+                  indicatorColor: Colors.black87, //tabbar 밑줄 색상
+                  labelColor: Colors.black87, //선택된 영역 글자 색
+                  unselectedLabelColor: Colors.grey, //선택 안된 영역 글자색
                   tabs: myTabs,
                 ),
               ),
