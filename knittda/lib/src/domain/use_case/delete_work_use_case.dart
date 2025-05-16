54b2ce035389c4ee08d219e6e6d2594c808c96bd
jungHY <jungHY@dankook.ac.kr>
2025-05-16 23:19:59 +0900

    feat: work_use_case 생성

---
+++ knittda/lib/src/domain/use_case/delete_work_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:knittda/src/data/repositories/work_repositories.dart';
+
+class DeleteWorkUseCase {
+  final WorkRepositories workRepositories;
+
+  DeleteWorkUseCase({
+    required this.workRepositories,
+  });
+
+  Future<void> call(String accessToken, int projectId) async {
+    try {
+      await workRepositories.deleteWork(accessToken, projectId);
+    } catch (e, stack) {
+      debugPrint('DeleteWorkUseCase 오류: $e\n$stack');
+      rethrow; // 호출 측에서 catch 가능하게 재던짐
+    }
+  }
+}
