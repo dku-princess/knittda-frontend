afad57cf853fe93018cb6d1e52ed74be71e5dcab
jungHY <jungHY@dankook.ac.kr>
2025-06-06 12:57:35 +0900

    feat: usecase, vm 추가

---
+++ knittda/lib/src/domain/use_case/get_report_use_case.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:knittda/src/data/models/report_model.dart';
+import 'package:knittda/src/data/repositories/report_repository.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+class GetReportUseCase {
+  final ReportRepository reportRepository;
+
+  GetReportUseCase({
+    required this.reportRepository,
+  });
+
+  Future<({ReportModel report})> call (String accessToken) async {
+    try {
+      final result = await reportRepository.getReport(accessToken);
+      return (report: result.report);
+    } catch (e, stack) {
+      await Sentry.captureException(e, stackTrace: stack);
+      debugPrint('GetReportUseCase 오류: $e\n$stack');
+      rethrow; // 호출 측에서 catch 가능하게 재던짐
+    }
+  }
+}
