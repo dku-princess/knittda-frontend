05ac7d2fac2f17830b7360b22c801bfd6762c5c8
aojowo <anjoungwo@gmail.com>
2025-07-11 15:39:22 +0900

    test2

--- knittda/lib/src/app.dart
+++ knittda/lib/src/app.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 //import 'package:knittda/src/presentation/screens/work_detail/report_ui.dart';
 // 우정우정스
 //깃크라켄 학습//
+//공부중
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
