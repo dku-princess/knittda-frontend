7e2690bb4fccc0bed030e09cb3fa2b15920c7e18
이지수 <jisoolee031@gmail.com>
2025-08-04 23:03:02 +0900

    fix. time duration

--- lib/src/presentation/screens/work_detail/show_record.dart
+++ lib/src/presentation/screens/work_detail/show_record.dart
@@ -67,9 +67,8 @@ class _ShowRecordState extends State<ShowRecord> {
       );
     }
 
-    final corrected = record.createdAt!.add(const Duration(hours: 9));
-    final dateStr = DateUtilsHelper.toDotFormat(corrected);
-    final timeStr = DateUtilsHelper.toHourMinuteFormat(corrected);
+    final dateStr = DateUtilsHelper.toDotFormat(record.createdAt!);
+    final timeStr = DateUtilsHelper.toHourMinuteFormat(record.createdAt!);
 
     return Stack(
       children: [
