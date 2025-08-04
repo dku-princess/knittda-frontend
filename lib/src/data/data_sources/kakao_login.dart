f7c4aad5b786617a25df017af7ef9362e3cb2f3e
이지수 <jisoolee031@gmail.com>
2025-08-05 00:06:56 +0900

    fix. add flutter web auth

--- lib/src/data/data_sources/kakao_login.dart
+++ lib/src/data/data_sources/kakao_login.dart
@@ -1,4 +1,7 @@
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:knittda/env.dart';
 
 import 'package:knittda/src/data/data_sources/social_login.dart';
 
@@ -57,16 +60,43 @@ class KaKaoLogin implements SocialLogin {
     }
   }
 
-  // 웹 로그인 전용 메서드 (iPhone mini 등에서 사용)
+  // 웹 로그인 전용 메서드 (iOS 17+ 충돌 문제 해결)
   Future<String?> loginWithWebOnly() async {
     try {
-      OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
-      //debugPrint('웹 로그인 성공 ${token.accessToken}');
+      final clientId = Env.kakaoNativeAppKey;
+      final redirectUri = 'kakao$clientId://oauth';
+
+      final url = Uri.https('kauth.kakao.com', '/oauth/authorize', {
+        'client_id': clientId,
+        'redirect_uri': redirectUri,
+        'response_type': 'code',
+        'prompt': 'login', // 항상 로그인창 표시
+      });
+
+      // Safari에서 명시적으로 OAuth를 수행합니다.
+      final result = await FlutterWebAuth.authenticate(
+        url: url.toString(),
+        callbackUrlScheme: 'kakao$clientId',
+      );
+
+      // 인증 코드 파싱
+      final code = Uri.parse(result).queryParameters['code'];
+      if (code == null) {
+        debugPrint('인증 코드를 받지 못했습니다');
+        return null;
+      }
+
+      // 카카오 서버에서 OAuth 토큰을 받아옴
+      OAuthToken token = await AuthApi.instance.issueAccessToken(code: code);
+
+      // 토큰 저장
+      await TokenManagerProvider.instance.manager.setToken(token);
+      
+      debugPrint('웹 로그인 성공 ${token.accessToken}');
       return token.accessToken;
     } catch (error) {
-      //debugPrint('웹 로그인 실패 $error');
+      debugPrint('웹 로그인 실패 $error');
       return null;
     }
   }
-
 }
