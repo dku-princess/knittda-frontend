29ea3b14bc0651c35a8fe1155c605a2951e1b372
jungHY <jungHY@dankook.ac.kr>
2025-12-18 00:18:45 +0900

    remove: admin 로직 삭제

--- lib/src/data/data_sources/authentication_api.dart
+++ lib/src/data/data_sources/authentication_api.dart
@@ -77,27 +77,6 @@ class AuthenticationApi {
     }
   }
 
-  //관리자 로그인
-  Future<Result<Map<String, dynamic>>> getAuthAdmin() async {
-    try {
-      final response = await _dio.get(
-        '/api/v1/auth/admin',
-      );
-
-      if (response.statusCode == 200) {
-        final data = response.data;
-        final Map<String, dynamic> hits = data['data'];
-        return Result.success(hits);
-      } else {
-        return Result.error('서버 오류: ${response.statusCode}');
-      }
-    } on DioException catch (e) {
-      return Result.error('네트워크 에러: ${e.message}');
-    } catch (e) {
-      return Result.error('알 수 없는 에러');
-    }
-  }
-
   //탈퇴
   Future<Result<void>> deleteAuthSignout() async {
     try {
