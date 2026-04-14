9bdca9c2050ba889f9243bcb97f6b3d5b5d7bdfa
junghy <junghy250@gmail.com>
2026-04-14 19:48:51 +0900

    feat: InAppBanner
    - fetchBottomBanners
    - getBottomBanners
    - InAppBanner model

---
+++ lib/src/data/data_sources/in_app_banner_api.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:knittda/src/domain/model/in_app_banner.dart';
+
+class InAppBannerApi {
+  final Dio _dio;
+
+  InAppBannerApi(this._dio);
+
+  Future<List<InAppBanner>> fetchBottomBanners() async {
+    final response = await _dio.get(
+      '/items/in_app_banner',
+      queryParameters: {
+        'filter[status][_eq]': 'published',
+        'filter[is_active][_eq]': true,
+        'filter[placement][_eq]': 'bottom',
+        'sort': 'priority',
+      },
+    );
+
+    final List<dynamic> data = response.data['data'];
+
+    return data.map((e) => InAppBanner.fromJson(e)).toList();
+  }
+
+  String getAssetUrl(String assetId) {
+    return '${_dio.options.baseUrl}/assets/$assetId';
+  }
+}
